4122d7725911e1d740d1df20b6f7061f8add38a6
Arkady Elterman <elterman@gmail.com>
2025-09-04 16:07:51 -0700

    .

--- src/Stats.svelte
+++ src/Stats.svelte
@@ -2,27 +2,33 @@
     import NumberFlow from '@number-flow/svelte';
     import { _stats } from './state.svelte';
 
-    const classes = $derived(`item ${_stats.reset ? 'reset' : ''} background-bronze`);
+    const classes = $derived(`item ${_stats.reset ? 'reset' : ''}`);
     const ave = $derived(_stats.ave());
 </script>
 
 <div class="stats">
     <div class={classes}>
-        puzzles  <div class="flow"><NumberFlow value={_stats.plays} /></div>
+        <span class="label">puzzles</span>
+        <div class="flow"><NumberFlow value={_stats.plays} /></div>
     </div>
-    {#if _stats.plays}
-        <div class={classes}>
-            ave  {`${ave < 1 ? '' : '+'}`}
+    <div class={classes}>
+        <span class="label">ave</span>
+        {#if _stats.plays}
+            {`${ave < 1 ? '' : '+'}`}
             <div class="flow"><NumberFlow value={ave} /></div>
-        </div>
-        <div class={classes}>
-            best  {`${_stats.best < 1 ? '' : '+'}`}
+        {:else}
+            <div class="flow">N/A</div>
+        {/if}
+    </div>
+    <div class={classes}>
+        <span class="label">best</span>
+        {#if _stats.plays}
+            {`${_stats.best < 1 ? '' : '+'}`}
             <div class="flow"><NumberFlow value={_stats.best} /></div>
-        </div>
-    {:else}
-        <div class={classes}>ave</div>
-        <div class={classes}>best</div>
-    {/if}
+        {:else}
+            <div class="flow">N/A</div>
+        {/if}
+    </div>
 </div>
 
 <style>
@@ -30,30 +36,34 @@
         grid-area: 1/1;
         display: grid;
         grid-auto-flow: column;
-        grid: auto / 1.3fr 1fr 1fr;
         height: 28px;
-        width: 90%;
+        gap: 20px;
         place-self: center;
     }
 
     .item {
         width: 94%;
         box-sizing: border-box;
-        font-family: SS3;
+        font-family: Trajan;
         font-size: 17px;
         border-radius: 50vh;
         height: 90%;
-        padding: 0 0 2px;
         display: grid;
         grid-auto-flow: column;
         align-items: center;
         place-content: center;
         place-self: center;
         transition: 0.2s;
+        color: var(--aqua);
     }
 
     .reset {
-        background: var(--gold);
+        /* background: var(--gold); */
+    }
+
+    .label {
+        color: var(--bronze);
+        margin-right: 10px;
     }
 
     .flow {
